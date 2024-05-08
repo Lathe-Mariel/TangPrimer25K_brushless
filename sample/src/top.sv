cba062b7a52b50447184c664eca6404d0cf8eb57
Lathe <n11akira@yahoo.co.jp>
2024-05-08 23:10:23 +0900

    0508

--- sample/src/top.sv
+++ sample/src/top.sv
@@ -3,9 +3,7 @@
 module top (
   input  wire       clk,
 
-  input  wire sw1,  //on board SW
-  input  wire sw2,  //on board SW
-  input wire[3:0] tacSW,  // tac SW1-4
+  input wire[1:0] tacSW,  // tac SW1-2
   input wire[2:0] toggleSW,  //toggle SW 1-3
   input  wire[2:0] HS,  //HallSensor
   output wire AD_CLK,  // for MCP3008(ADC)
@@ -18,9 +16,8 @@ module top (
   output logic _LIN_R,  //Lower arm
   output logic _LIN_S,
   output logic _LIN_T,
-  output logic[7:0] anode,  //7seg LED
-  output logic[3:0] cathode,  //7seg LED
-  output wire[5:0] boardLED  //on board_LED
+  output logic CAN_LED0,
+  output logic CAN_LED1
 );
 
   logic  controlCLK;
@@ -37,15 +34,10 @@ module top (
   logic isRotate;  // for control forcedRotation
   logic[2:0] oldHS;  // old Hall Sensor value
 
-  logic[15:0] display7seg; //0000-9999
-  logic[1:0] disp_digit;  // display digit of 7seg LED
-  logic[1:0] disp_state;  //0:volume, 1:duty, 2:HS speed, 3:
-  logic sw1pushed;  // flag for on_board_sw1 pushed
-  logic[3:0] tacSWpushed; // flag for tac_SW1-4
+  logic[1:0] tacSWpushed; // flag for tac_SW1-4
 
   logic[9:0] recieveADC;  // adc data from MCP3008
   logic[9:0] accel;  // accel value, that is transformed from recieveADC
-  logic[9:0] disp_speed;  // store rotation speed for display
 
   logic[11:0] dutyList[8]={'d1400, 'd1000, 'd800, 'd700, 'd620, 'd560, 'd520, 'd500};
   logic[2:0] dutyPara;
@@ -86,9 +78,7 @@ module top (
 
 // check sw //change something to display
     if(processCounter % 2048 == 0)begin
-      if(sw1 != sw1pushed && sw1pushed == 1)begin
-        disp_state <= disp_state + 'd1;
-      end else if(tacSW[3] != tacSWpushed[3] && tacSWpushed[3] == 1)begin
+      if(tacSW[1] != tacSWpushed[1] && tacSWpushed[1] == 1)begin
         dutyPara <= dutyPara + 'd1;
       end else begin
         if(HSCounter > 42)begin
@@ -110,8 +100,7 @@ module top (
         end
 
       end
-      sw1pushed <= sw1;
-      tacSWpushed[3] <= tacSW[3];
+      tacSWpushed[1] <= tacSW[1];
 
 // measure speed
 
@@ -120,7 +109,6 @@ module top (
       end else begin
         isRotate <= 'b0;
       end
-      disp_speed <= HSCounter;
       HSCounter <= 0;
     end else begin
       if(oldHS != HS)begin
@@ -171,29 +159,6 @@ module top (
 
   end
 
-// 7seg control
-  logic[9:0] divider;
-  always @(posedge processCounter[3])begin
-    case(disp_state)
-      2'd0: display7seg <= accel;
-      2'd1: display7seg <= accel/'d16; // duty
-      2'd2: display7seg <= disp_speed;  // to show motor rotation speed
-      2'd3: display7seg <= dutyPara;  // to show number of current pwm frequency
-    endcase
-
-    disp_digit <= disp_digit + 1;
-    cathode <= toggleSW[1] * 4'b0001 << disp_digit;
-
-    case(disp_digit)
-      2'd0: divider = 1;
-      2'd1: divider = 10;
-      2'd2: divider = 100;
-      2'd3: divider = 1000;
-    endcase
-
-    anode <= decode7seg((display7seg/divider) % 10);
-  end
-
 //120 square pulse drive
   always @(rotateState)begin
     if(toggleSW[2])begin
@@ -211,38 +176,13 @@ module top (
   end
 
   assign AD_CLK = controlCLK;
-  assign boardLED[2:0] = HS;
-  assign boardLED[5] = toggleSW[0];
-  assign boardLED[4] = toggleSW[1];
-  assign boardLED[3] = toggleSW[2];
+
+  assign CAN_LED0 = processCounter[11];
+  assign CAN_LED1 = ~processCounter[12];
 
   assign _LIN_R = ~(~_LR * duty);
   assign _LIN_S = ~(~_LS * duty);
   assign _LIN_T = ~(~_LT * duty);
-
-
-  function [7:0] decode7seg;
-  input [3:0] in;
-    case(in)
-      4'h0:  decode7seg = 8'b00000011;
-      4'h1:  decode7seg = 8'b10011111;
-      4'h2:  decode7seg = 8'b00100101;
-      4'h3:  decode7seg = 8'b00001101;
-      4'h4:  decode7seg = 8'b10011001;
-      4'h5:  decode7seg = 8'b01001001;
-      4'h6:  decode7seg = 8'b01000001;
-      4'h7:  decode7seg = 8'b00011111;
-      4'h8:  decode7seg = 8'b00000001;
-      4'h9:  decode7seg = 8'b00001001;
-      4'ha:  decode7seg = 8'b00010001;
-      4'hb:  decode7seg = 8'b11000001;
-      4'hc:  decode7seg = 8'b01100011;
-      4'hd:  decode7seg = 8'b10000101;
-      4'he:  decode7seg = 8'b01100001;
-      4'hf:  decode7seg = 8'b01110001;
-      default:decode7seg = 8'b11111111;
-    endcase
-  endfunction
 
 
 // generate pulse
@@ -251,7 +191,7 @@ module top (
   logic [11:0] counter = 'd0;
   logic [11:0] counterB = 'd0;
 
-  always_ff @ (posedge clk) begin
+  always_ff @(posedge clk) begin
     if(counter == COUNT_MAX)begin
       counter  <= 'd0;
     end else if (counter < COUNT_MAX/2) begin
