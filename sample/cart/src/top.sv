f13bade841f576bed0136a7417c02ca8016781b4
Lathe <nagai@cqpub.co.jp>
2024-05-25 19:58:53 +0900

    0525

--- sample/cart/src/top.sv
+++ sample/cart/src/top.sv
@@ -19,11 +19,34 @@ module top (
   output logic _LIN_T,
   output logic CAN_LED0,
   output logic CAN_LED1,
-  output logic CAN_S,
-  output logic CAN_TX,
-  input wire CAN_RX,
+  output logic s,     //CAN Mode Select
+  output logic txd,   //CAN データ送信線
+  input wire rxd,     //CAN データ受信線
   output logic CAN_WS
 );
+
+  localparam CLK_FREQ_HZ    = 50_000_000;         // 入力クロック周波数
+  localparam CAN_BITRATE_HZ = 500_000;            // CAN のビットレート
+  localparam SLEEP_CYCLE    = CLK_FREQ_HZ / 100;  // データ送信後、スリープするcycle 数
+  localparam ID_ENGINE_REV  = 11'h3D9;            // エンジン回転数送信時のID
+  localparam ID_CAR_SPEED   = 11'h3E9;            // 車速送信時のID
+
+  // Normal mode に固定
+  assign s = 1'b0;
+
+  // モジュール間の接続に使用する変数
+  wire status_warning;
+  wire status_bus_off;
+  logic [13:0] engine_rev;
+  wire [8:0]  vehicle_speed;
+  wire [63:0] stm_send_data_tdata;
+  wire [10:0] stm_send_data_tid;
+  wire [7:0]  stm_send_data_tkeep;
+  wire        stm_send_data_tvalid;
+  wire        stm_send_data_tready;
+  wire [2:0]  stm_result_tdata;
+  wire        stm_result_tvalid;
+  wire        stm_result_tready;
 
   logic  controlCLK;
   logic rotateCLK;
@@ -46,6 +69,40 @@ module top (
 
   logic[11:0] dutyList[8]={'d1400, 'd1000, 'd800, 'd700, 'd620, 'd560, 'd520, 'd500};
   logic[2:0] dutyPara;
+
+///////// エンジン回転数と車速を送信するモジュール /////////
+  vehicle_data_generator #(
+    .SLEEP_CYCLE(SLEEP_CYCLE),
+    .ID_ENGINE_REV(ID_ENGINE_REV),
+    .ID_CAR_SPEED(ID_CAR_SPEED)
+  ) vehicle_data_generator_i (
+    .stm_send_data_out_tdata (stm_send_data_tdata),
+    .stm_send_data_out_tid   (stm_send_data_tid),
+    .stm_send_data_out_tkeep (stm_send_data_tkeep),
+    .stm_send_data_out_tvalid(stm_send_data_tvalid),
+    .stm_send_data_out_tready(stm_send_data_tready),
+    .stm_result_in_tdata     (stm_result_tdata),
+    .stm_result_in_tvalid    (stm_result_tvalid),
+    .stm_result_in_tready    (stm_result_tready),
+    .*
+  );
+
+  ///////// CAN コントローラ /////////
+  can_controller #(
+    .CLK_FREQ_HZ(CLK_FREQ_HZ),
+    .CAN_BITRATE_HZ(CAN_BITRATE_HZ)
+  ) can_controller_i (
+    .stm_send_data_in_tdata (stm_send_data_tdata),
+    .stm_send_data_in_tid   (stm_send_data_tid),
+    .stm_send_data_in_tkeep (stm_send_data_tkeep),
+    .stm_send_data_in_tvalid(stm_send_data_tvalid),
+    .stm_send_data_in_tready(stm_send_data_tready),
+    .stm_result_out_tdata   (stm_result_tdata),
+    .stm_result_out_tvalid  (stm_result_tvalid),
+    .stm_result_out_tready  (stm_result_tready),
+    .*
+  );
+
 
   always @(posedge controlCLK)begin
 
@@ -103,6 +160,8 @@ module top (
         end else begin
           dutyPara <= 'd0;
         end
+
+        engine_rev <= HSCounter * 10'd10;
 
       end
       tacSWpushed[1] <= tacSW[1];
