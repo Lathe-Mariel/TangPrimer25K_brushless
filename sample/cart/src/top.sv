cc3fd2238f064e02b443a1e4a880e23bf687c97d
Lathe <nagai@cqpub.co.jp>
2024-06-10 22:08:55 +0900

    0610

--- sample/cart/src/top.sv
+++ sample/cart/src/top.sv
@@ -220,14 +220,14 @@ module top (
       CS <= 1;
     end else begin
 
-//      vehicle_speed <= analog_scan[0] >> 2;            // for MCP3008 0ch(C7-28)
-      if(analog_scan[0] < 'd250)begin
+//      vehicle_speed <= analog_scan[0] >> 2;            // for MCP3008 0ch(C7-28) 7ch(C10-28)
+      if(analog_scan[7] < 'd750)begin
         vehicle_speed <= 'd0;
       end else begin
-        vehicle_speed <= (analog_scan[0] - 'd250) >> 2 ;  // for 
+        vehicle_speed <= (analog_scan[7] - 'd750) >> tacSW  ;  // for 
       end
 
-      battery_value <= analog_scan[2] >> (tacSW + 4) ;         // for MCP3008 2ch(analog input)
+      battery_value <= analog_scan[2] >> 6 ;         // for MCP3008 2ch(analog input)
 
       if(analog_scan[5] < 'd280)begin
         accel <= 'd0;
