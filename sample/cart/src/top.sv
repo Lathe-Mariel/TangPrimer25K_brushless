1935f2055eec4ab70f0e623885b402be0783239d
Lathe <nagai@cqpub.co.jp>
2025-09-25 19:31:15 +0900

    start parameter

--- sample/cart/src/top.sv
+++ sample/cart/src/top.sv
@@ -259,10 +259,10 @@ module top (
       end else if(analog_scan[5] > 'd780) begin
         accel <= 'd1000;
       end else begin
-        if(HSCounter < 12)begin  //soft start
-          accel <= (((analog_scan[5] - 'd280) * 2) < 60) ?(analog_scan[5] - 'd280) * 2 : 'd60;
-//        end else if(HSCounter < 28)begin //soft start2
-//          accel <= (((analog_scan[5] - 'd280) * 2) < 100) ?(analog_scan[5] - 'd280) * 2 : 'd100;
+        if(HSCounter < 6)begin  //soft start
+          accel <= (((analog_scan[5] - 'd280) * 2) < 100) ?(analog_scan[5] - 'd280) * 2 : 'd100;
+        end else if(HSCounter < 18)begin //soft start2
+          accel <= (((analog_scan[5] - 'd280) * 2) < 200) ?(analog_scan[5] - 'd280) * 2 : 'd200;
         end else begin
           accel <= (analog_scan[5] - 'd280) * 2;  // for Mini Cart Accel     //origin 270 - 780
         end
